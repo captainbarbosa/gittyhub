018e1b5d407aee5d4aa1cbdc3ff03595171ff45c
Nadia Barbosa <nadiabarbosa@me.com>
2016-04-06 00:27:58 -0400

    Stopping point for now

--- app.rb
+++ app.rb
@@ -5,16 +5,21 @@ require "json"
 require "pry"
 require "./github"
 
-get "/" do
+get "/lookup" do
   render_template("views/home.html.erb")
 end
 
 post "/lookup" do
-  uri = URI("https://api.github.com/users/captainbarbosa")
-  @person = GithubClient.new(uri, :get)
-  @person.body
-  render_template("views/profile.html.erb")
+  username_uri = URI("https://api.github.com/users/#{params[:username]}")
+  repos_uri = URI("https://api.github.com/users/#{params[:username]}/repos")
 
+  user = GithubClient.new(username_uri, :get)
+  repos = GithubClient.new(repos_uri, :get)
+
+  @user = JSON.parse(user.body)
+  @repos = JSON.parse(repos.body)
+
+  render_template("views/profile.html.erb")
 end
 
 def render_template(template_path)
--- tests/app_tests.rb
+++ tests/app_tests.rb
@@ -7,18 +7,27 @@ class AppTest < Minitest::Test
     Sinatra::Application
   end
 
-  def test_declares_its_name
-    response = get "/"
+  def test_lookup_page_returns_content
+    response = get "/lookup"
+
     assert response.ok?
-    assert_equal "I am Groot", response.body
+    assert_equal false, response.body.empty?
   end
 
-  def test_it_handles_and_returns_json
-    hash = { name: "bob" }
-    response = post("/employees", hash.to_json, { "CONTENT_TYPE" => "application/json" })
+  def test_github_api_returns_username
+    uri = URI("https://api.github.com/users/captainbarbosa")
+    person = GithubClient.new(uri, :get)
+    person = JSON.parse(person.body)
+    username = person["login"]
 
-    assert response.ok?
-    payload = JSON.parse(response.body)
-    assert_equal({ "name" => "bob" }, payload)
+    assert_equal false, username.empty?
+  end
+
+  def test_github_api_returns_users_repos
+    uri = URI("https://api.github.com/users/captainbarbosa/repos")
+    repos = GithubClient.new(uri, :get)
+    repos = JSON.parse(repos.body)
+
+    assert_equal true, repos.is_a?(Array)
   end
 end
